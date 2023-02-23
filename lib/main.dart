2663e6618032e303658bcc8bedf6dc4eb55cb951
Chris Oh <chris@5tigerjelly.com>
2023-02-22 21:44:12 -0500

    convert to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,41 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    print('Answer chosen');
+    setState(() {
+      _questionIndex += 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ["Favorite Color?", "Favorite animal?"];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text(questions.elementAt(_questionIndex)),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 1')),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 2')),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 3')),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 4')),
+          ],
+        ),
       ),
     );
   }
